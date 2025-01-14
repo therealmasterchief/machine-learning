2b68ab8ae34c748c38b7398a780574c1081ea224
therealmasterchief <flamethegamer26@gmail.com>
2025-01-13 21:02:58 -0400

    or

---
+++ efghijklml.py
@@ -0,0 +1,11 @@
+num1=10
+num2=4
+
+print("and op:",num1&num2)
+print("or op:",num1|num2)
+print("ex or op:", num1^num2)
+
+print("not op:",~num1)
+
+print("right shift", num1>>1)
+print("left shift",num1<<1)
