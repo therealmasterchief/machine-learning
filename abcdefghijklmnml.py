75ae6cb20c32b199d0af926f8a992fda12687b94
therealmasterchief <flamethegamer26@gmail.com>
2025-01-20 21:12:14 -0400

    equalnot

--- abcdefghijklmnml.py
+++ abcdefghijklmnml.py
@@ -1,9 +1,9 @@
 
 def Checkifsame(number1,number2):
     if(number1 ^ number2 !=0):
-        print("numbers are equal")
+        print("numbers are not equal")
     else:
-        print("Numbers are not equal")
+        print("Numbers are equal")
 n1 =int(input("1st number: "))
 n2 =int(input("2nd number:"))
 Checkifsame(n1,n2)
